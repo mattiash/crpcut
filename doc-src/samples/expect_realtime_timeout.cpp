7d472a9e343e2ab6eb10e7acfec39b9636cb77ba
Bjorn Fahller <bjorn@fahller.se>
2010-10-25 10:47:42 +0200

    Documented EXPECT_REALTIME_TIMEOUT_MS

---
+++ doc-src/samples/expect_realtime_timeout.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2010 Bjorn Fahller <bjorn@fahller.se>
+ * All rights reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <crpcut.hpp>
+
+extern "C"
+{
+#include <signal.h>
+}
+
+TEST(fail_too_quick, EXPECT_REALTIME_TIMEOUT_MS(100))
+{
+}
+
+TEST(pass_busy_wait, EXPECT_REALTIME_TIMEOUT_MS(100))
+{
+  for (;;)
+    ;
+}
+
+TEST(fail_busy_wait, EXPECT_REALTIME_TIMEOUT_MS(100), DEADLINE_CPU_MS(3))
+{
+  for (;;)
+    ;
+}
+
+TEST(pass_sleep, EXPECT_REALTIME_TIMEOUT_MS(100), DEADLINE_CPU_MS(3))
+{
+  sleep(1);
+}
+
+int main(int argc, char *argv[])
+{
+  return crpcut::run(argc, argv);
+}
